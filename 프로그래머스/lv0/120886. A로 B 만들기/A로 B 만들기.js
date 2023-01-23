ed5efc5dce2867f1e0415408baf22389874dc26f
Hyejis <112938316+saramnim@users.noreply.github.com>
2023-01-23 17:15:26 +0900

    [level 0] Title: A로 B 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,9 +1,12 @@
 function solution(before, after) {
-    var bef = [...before].sort().join();
-    var aft = [...after].sort().join();
-    if(bef===aft){
-        return 1;
-    }else{
-        return 0;
-    }
+    // var bef = [...before].sort().join();
+    // var aft = [...after].sort().join();
+    // if(bef===aft){
+    //     return 1;
+    // }else{
+    //     return 0;
+    // }
+    return [...before].sort()
+    .filter((el,i) => el !== [...after].sort()[i]).length 
+    ? 0 : 1
 }
