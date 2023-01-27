03789a341ecc59c61edb5319d4043a95859e4e96
Hyejis <112938316+saramnim@users.noreply.github.com>
2023-01-28 00:03:32 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = [];
+    for(let i=2;i<=n;i++){
+        if(n%i===0){
+            while(n % i==0) n = n / i
+            answer.push(i)
+        }
+    }
+    return answer;
+}
