ec91cfaa49b86d3bf22e5e7b9538726cd1348635
Hyejis <112938316+saramnim@users.noreply.github.com>
2023-02-03 23:00:19 +0900

    [level 0] Title: 합성수 찾기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    var answer = new Set();
+    for(let i=4;i<=n;i++){
+        for(let j=2;j<i;j++){
+            if(i%j===0) answer.add(i)
+        }
+    }
+    return answer.size;
+}
