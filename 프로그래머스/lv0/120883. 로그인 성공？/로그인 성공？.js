b2965f8724fba62af9e021277556d23f15448cab
Hyejis <112938316+saramnim@users.noreply.github.com>
2023-02-06 22:57:36 +0900

    [level 0] Title: 로그인 성공?, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,16 +1,25 @@
+// function solution(id_pw, db) {
+//     var  answer="";
+//     // for(let i=0; i<db.length;i++){
+//     //     db[i][0]===id_pw[0] && db[i][1] === id_pw[1] ? 
+//     //         answer= "login" : 
+//     //     db[i][0]===id_pw[0] && db[i][1] !== id_pw[1] ? 
+//     //         answer= "wrong pw" : 
+//     //     answer= "fail"
+//     // }
+//     db.map((arr)=>{
+//         if(arr[0]===id_pw[0]&&arr[1]===id_pw[1]) answer= "login"
+//         else if(arr[0]===id_pw[0] && arr[1] !== id_pw[1]) answer= "wrong pw" 
+//         else if(arr[0]!==id_pw[0] && arr[1] !== id_pw[1]) answer= "fail"
+//     })
+//     return answer;
+// }
 function solution(id_pw, db) {
     var  answer="";
-    // for(let i=0; i<db.length;i++){
-    //     db[i][0]===id_pw[0] && db[i][1] === id_pw[1] ? 
-    //         answer= "login" : 
-    //     db[i][0]===id_pw[0] && db[i][1] !== id_pw[1] ? 
-    //         answer= "wrong pw" : 
-    //     answer= "fail"
-    // }
-    db.map((arr)=>{
-        if(arr[0]===id_pw[0]&&arr[1]===id_pw[1]) answer= "login"
-        else if(arr[0]===id_pw[0] && arr[1] !== id_pw[1]) answer= "wrong pw" 
-        else if(arr[0]!==id_pw[0] && arr[1] !== id_pw[1]) answer= "fail"
+    db.map((arr, i)=>{
+        (arr[0]===id_pw[0]&&arr[1]===id_pw[1]) ? answer= "login" :
+          (arr[0]!==id_pw[0] && arr[1] !== id_pw[1]) ? answer= "fail" :
+        answer= "wrong pw"
     })
     return answer;
-}
+}  
