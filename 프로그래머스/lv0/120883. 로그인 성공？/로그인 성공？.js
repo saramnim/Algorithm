d7d4f6f49625ca727e856580e4c637859f5454a9
Hyejis <112938316+saramnim@users.noreply.github.com>
2023-02-06 22:44:16 +0900

    [level 0] Title: 로그인 성공?, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,16 @@
+function solution(id_pw, db) {
+    var  answer="";
+    // for(let i=0; i<db.length;i++){
+    //     db[i][0]===id_pw[0] && db[i][1] === id_pw[1] ? 
+    //         answer= "login" : 
+    //     db[i][0]===id_pw[0] && db[i][1] !== id_pw[1] ? 
+    //         answer= "wrong pw" : 
+    //     answer= "fail"
+    // }
+    db.map((arr, i)=>{
+        if(arr[0]===id_pw[0]&&arr[1]===id_pw[1]) answer= "login"
+        else if(arr[0]===id_pw[0] && arr[1] !== id_pw[1]) answer= "wrong pw" 
+        else if(arr[0]!==id_pw[0] && arr[1] !== id_pw[1]) answer= "fail"
+    })
+    return answer;
+}
