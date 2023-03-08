a3e1baffdd767ff857c9e2c54e6eed197906aff8
Hyejis <112938316+saramnim@users.noreply.github.com>
2023-03-08 17:19:47 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -4,6 +4,8 @@ function solution(a, b) {
 }
 
 // function solution(a, b) {
-//     var answer = b % 2 === 0 || b % 5 === 0 ? 1 : 2
+//     var answer = b % 2 === 0 || b % 5 === 0 
+//     && (a !== 0 || b!== 0) 
+//     ? 1 : 2
 //     return answer;
 // }
