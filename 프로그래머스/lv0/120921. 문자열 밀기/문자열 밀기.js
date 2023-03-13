2dcadf901f9b8bc9e94f1d6a42c9335ac4a8c9a0
Hyejis <112938316+saramnim@users.noreply.github.com>
2023-03-13 23:20:53 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,18 +1,4 @@
-// function solution(A, B) {
-//     var a = [...A];
-//     for(let i = 0; i < A.length; i++){
-//         if(A === B)
-//             return i;
-//         else{
-//             a.unshift(a.pop())
-//             if(a.join('') === B)
-//                 return i + 1
-//         }
-//     }
-//     return -1;
-// }
 function solution(A, B) {
     var str = B + B;
-    console.log(str)
     return str.indexOf(A)
 }
