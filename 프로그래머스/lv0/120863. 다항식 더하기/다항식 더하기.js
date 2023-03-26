296cb3886179bc43efcf3af8af8a034c3e9592aa
Hyejis <112938316+saramnim@users.noreply.github.com>
2023-03-26 18:06:32 +0900

    [level 0] Title: 다항식 더하기, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -2,10 +2,10 @@ function solution(polynomial) {
     var answer = [];
     var arr = polynomial.split(" ");
     var xarr = [];
-    var numArr = 0;
+    var num = 0;
     for(let i = 0; i < arr.length; i++){
         if(arr[i].split("").includes("x")) xarr.push(arr[i])
-        else if(arr[i] !== "+") numArr+=(Number(arr[i]))
+        else if(arr[i] !== "+") num+=(Number(arr[i]))
     }
     if(xarr.length){
         xarr = xarr.map(v=> v.split("x").map(v2 => v2 === "" ? v2 = 1 : v2))
@@ -13,6 +13,6 @@ function solution(polynomial) {
         xarr = xarr.reduce((acc,cur) => acc + cur)
         xarr !== 1 ? answer.push(xarr+"x") : answer.push("x")
     }
-    numArr !== 0 ? answer.push(numArr) : 0
+    num !== 0 ? answer.push(num) : 0
     return answer.join(" + ").toString();
 }
