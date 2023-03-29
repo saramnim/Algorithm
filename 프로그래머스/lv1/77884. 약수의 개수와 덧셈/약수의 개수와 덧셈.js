97ad9f4630780b1365b857472cfc2f8678873b90
Hyejis <112938316+saramnim@users.noreply.github.com>
2023-03-29 17:42:33 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.49 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,11 @@
+function solution(left, right) {
+    var answer = 0;
+    for(let i = left; i <= right; i++){
+        let num = [];
+        for(let j = 1; j <= i; j++){
+            if(i % j === 0) num.push(j)
+        }
+        num.length % 2 === 0 ? answer+=i : answer-=i
+    }
+    return answer;
+}
