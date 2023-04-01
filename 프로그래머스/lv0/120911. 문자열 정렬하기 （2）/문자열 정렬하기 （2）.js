4564ca2814e2f37dbd33131a74305a8285982dcc
Hyejis <112938316+saramnim@users.noreply.github.com>
2023-04-01 21:58:14 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -1,4 +1,3 @@
 function solution(my_string) {
-    var answer = String(my_string.toLowerCase().split("").sort().join(""));
-    return answer;
+    return String(my_string.toLowerCase().split("").sort().join(""));;
 }
