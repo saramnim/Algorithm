1d69530be63551578b663e10f9065542d587e0cc
Hyejis <112938316+saramnim@users.noreply.github.com>
2023-04-02 18:33:28 +0900

    [level 0] Title: 가까운 수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,6 +1,6 @@
 function solution(array, n) {
-    var answer = array
-    .filter(v => Math.abs(v-n)===Math.min(...array.map(v=>Math.abs(v-n))))
+    return array
+    .filter(v => Math.abs(v-n)===Math.min(
+        ...array.map(v=>Math.abs(v-n))))
     .sort((a,b) => a-b)[0];
-    return answer
 }
